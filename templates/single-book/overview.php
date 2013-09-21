7250364da23903bae8d0da96b17f044d31b0feec
sdellacqua <silviodellacqua@gmail.com>
2013-09-21 14:22:04 +0200

    Update overview.php
    internationalizated

--- templates/single-book/overview.php
+++ templates/single-book/overview.php
@@ -1,4 +1,4 @@
 <div class="mbt-book-overview">
-	<h3 class="mbt-book-overview-title">Book Overview</h3>
+	<h3 class="mbt-book-overview-title"><?php _e('Book Overview', 'mybooktable'); ?></h3>
 	<?php global $post; echo(apply_filters("the_content", $post->post_content)); ?>
 </div>
